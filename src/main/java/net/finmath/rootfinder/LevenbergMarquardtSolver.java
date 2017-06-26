e2f8119a01671ec2d63d149f525f4f23b59c3c74
Stefan Sedlmair <stefan.sedlmair@campus.lmu.de>
2017-06-26 20:01:20 +0200

    Working Version of the Levenberg-Marquardt Alogrithm for RandomVariables
    Not yet parallelized!

--- src/main/java/net/finmath/rootfinder/LevenbergMarquardtSolver.java
+++ src/main/java/net/finmath/rootfinder/LevenbergMarquardtSolver.java
@@ -1,90 +1,166 @@
-/**
+ /**
  * 
  */
 package net.finmath.rootfinder;
 
-import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
+import java.util.TreeMap;
 
-import net.finmath.montecarlo.automaticdifferentiation.RandomVariableDifferentiableInterface;
+import net.finmath.functions.LinearAlgebra;
+import net.finmath.montecarlo.AbstractRandomVariableFactory;
+import net.finmath.montecarlo.RandomVariableFactory;
 import net.finmath.stochastic.RandomVariableInterface;
 
 /**
  * @author Stefan Sedlmair
- *
+ * @version 0.1
  */
-public abstract class LevenbergMarquardtSolver implements RandomVariableRootFinderUsingDerivative {
+public class LevenbergMarquardtSolver implements RandomVariableDifferentiableMultiDimensionalRootFinderInterface {
 
-	private final RandomVariableDifferentiableInterface initialValue;
-	private final RandomVariableInterface targetValue;
-	private final RandomVariableInterface uncertainties;
+	private final TreeMap<Long, RandomVariableInterface> initialValue; /* \beta_0 		*/
 	
-	private RandomVariableInterface	nextPoint;											// Stores the next point to be returned by getPoint()
-	private RandomVariableInterface	bestPoint;	
-	
-	private int	numberOfIterations				= 0;           							// Number of numberOfIterations
-	private boolean	isDone						= false;             					// Will be true if machine accuracy has been reached
-	private double accuracy 					= Double.MAX_VALUE;		
-	
-	public LevenbergMarquardtSolver(RandomVariableDifferentiableInterface initialValue, RandomVariableInterface targetValue, RandomVariableInterface uncertainties) {
-		super();		
-		this.initialValue 	= initialValue;
-		this.targetValue 	= targetValue;
-		this.uncertainties 	= uncertainties;
+	private TreeMap<Long, RandomVariableInterface>	nextParameterSet;	/* \beta_i 		*/	// Stores the next point to be returned by getPoint()
+	private TreeMap<Long, RandomVariableInterface>	bestParameterSet;	/* \beta_{best} */
 		
-		bestPoint 			= this.initialValue;
-		nextPoint 			= this.initialValue;
+	private int			numberOfIterations		= 0;           								// Number of numberOfIterations
+	private boolean		isDone					= false;             						// Will be true if machine accuracy has been reached
+	private double 		accuracy 				= Double.MAX_VALUE;		
+	
+	private double			 	lambda	 			= 0.001;
+	private final double		lambdaDivisor		= 1.3;
+	private final double		lambdaMultiplicator	= 2.0;
+	
+	/* predefine how to generate random variables */
+	private AbstractRandomVariableFactory randomVariableFactory = new RandomVariableFactory();
+	
+	/* predefine variables to  */
+	private final RandomVariableInterface targetFunctionValue;						/* y 			*/
+	private final RandomVariableInterface uncertainties;							/* \sigma 		*/
+
+	public LevenbergMarquardtSolver(TreeMap<Long, RandomVariableInterface> initialValue, RandomVariableInterface targetFunctionValue,
+			RandomVariableInterface uncertainties) {
+		super();
+		this.initialValue = initialValue;
+		
+		this.nextParameterSet = initialValue;
+		this.bestParameterSet = initialValue;
+		
+		this.targetFunctionValue = targetFunctionValue;
+		this.uncertainties = uncertainties;
+	}
+	
+	public LevenbergMarquardtSolver(TreeMap<Long, RandomVariableInterface> initialValue, RandomVariableInterface targetFunctionValue){
+		super();
+		this.initialValue = initialValue;
+		
+		this.nextParameterSet = initialValue;
+		this.bestParameterSet = initialValue;
+		
+		this.targetFunctionValue = targetFunctionValue;
+		this.uncertainties = randomVariableFactory.createRandomVariable(1.0);
 	}
 
-	public abstract RandomVariableDifferentiableInterface functionToMinimize(RandomVariableInterface[] arguments);
-	
-	/* (non-Javadoc)
-	 * @see net.finmath.rootfinder.RandomVariableRootFinderUsingDerivative#getNextPoint()
-	 */
-	@Override
-	public RandomVariableInterface getNextPoint() {
-		return null;
-
-	}
-	
-	private RandomVariableInterface estimateDelta(Map<Long, RandomVariableInterface> gradient, RandomVariableInterface lambda){
+	/**
+	 * implements 
+	 * \[ 
+	 * \left[ J^TJ +\lambda\cdot\text{diag}(J^TJ)\right]\delta = J^T\left[y - f(x,\beta) \right]
+	 * \]
+	 * and solves the equation for $\delta$.
+	 * 
+	 * @param gradient map of all partial derivative with respect to all dependent random variables, whose IDs are the keys in this map
+	 * @param lambda parameter associated with the step size of the iteration
+	 * */
+	private Map<Long, RandomVariableInterface> estimateDelta(RandomVariableInterface currentFunctionValue, Map<Long, RandomVariableInterface> gradient){
 			
+		// set up variables
 		int numberOfVariables = gradient.size();
-		int numberOfRealizations = gradient.values().toArray().length; /* all gradients should have the same size! */
+		int numberOfRealizations = currentFunctionValue.size();
 		
-		double[][] A = new double[numberOfVariables][numberOfRealizations];
-		double[] b = new double[numberOfRealizations];
+		// change from Maps to Arrays for solving the linear equation system
+		RandomVariableInterface[] gradientArray = new RandomVariableInterface[numberOfVariables];
+					
+		for(int variableIndex = 0; variableIndex < numberOfVariables; variableIndex++){
+			long key = gradient.keySet().iterator().next();
+			gradientArray[variableIndex] 	= gradient.get(key);
+		}
 		
+		RandomVariableInterface currentError = targetFunctionValue.sub(currentFunctionValue).div(uncertainties);
 		
-		for(int realizationIndex = 0; realizationIndex < numberOfRealizations; realizationIndex++){
-			for(int variableIndex = 0; variableIndex < numberOfVariables; variableIndex++){
+		double[][][] A = new double[numberOfRealizations][numberOfVariables][numberOfVariables];
+		double[][] b = new double[numberOfRealizations][numberOfVariables];
+		
+		/* A is symmetric thus only calculate the upper half */
+		for(int xIndex = 0; xIndex < numberOfVariables; xIndex++){			
+			for(int yIndex = xIndex; yIndex < numberOfVariables; yIndex++){
+		
 				
+				RandomVariableInterface alpha = gradientArray[xIndex].mult(gradientArray[yIndex]).mult(xIndex == yIndex ? 1.0 + lambda : 1.0);	
+				
+				for(int zIndex = 0; zIndex < numberOfRealizations; zIndex++){
+					A[zIndex][xIndex][yIndex] = alpha.get(zIndex);
+					A[zIndex][yIndex][xIndex] = alpha.get(zIndex);
+				}
+			}
+
+			/* b = J^T\left[y - f(x,\beta) \right]*/
+			RandomVariableInterface beta = gradientArray[xIndex].mult(currentError);
+			for(int zIndex = 0; zIndex < numberOfRealizations; zIndex++){
+				b[zIndex][xIndex] = beta.get(zIndex);
 			}
 		}
 		
-		return null;
-	}
+		double[][] deltaArray = new double[numberOfRealizations][numberOfVariables];
 
+		// TODO: possibly parallelisable
+		for(int zIndex = 0; zIndex < numberOfRealizations; zIndex++)
+			deltaArray[zIndex] = LinearAlgebra.solveLinearEquationSymmetric(A[zIndex], b[zIndex]);
+		
+		// re-associate the deltas with their parameter ids
+		TreeMap<Long, RandomVariableInterface> delta = new TreeMap<>();
+		int variableIndex = 0;
+		for(Long key:gradient.keySet()){
+			
+			double[] deltaPerRealizations = new double[numberOfRealizations];
+			for(int zIndex = 0; zIndex < numberOfRealizations; zIndex++)
+				deltaPerRealizations[zIndex] = deltaArray[zIndex][variableIndex];			
+			
+			delta.put(key, randomVariableFactory.createRandomVariable(gradient.get(key).getFiltrationTime(), deltaPerRealizations));
+			
+			variableIndex++;
+		}
+		
+		return delta;
+	}
 	/* (non-Javadoc)
-	 * @see net.finmath.rootfinder.RandomVariableRootFinderUsingDerivative#setValueAndDerivative(net.finmath.stochastic.RandomVariableInterface, net.finmath.stochastic.RandomVariableInterface)
+	 * @see net.finmath.rootfinder.RandomVariableRootFinderUsingDerivative#setValueAndDerivative()
 	 */
 	@Override
-	public void setValueAndDerivative(RandomVariableInterface value, RandomVariableInterface derivative) {
-		
-		double currentAccuracy = targetValue.sub(value).div(uncertainties).squared().getAverage() * targetValue.size();
-		
-		if(currentAccuracy < accuracy)
+    public void setValueAndDerivative(RandomVariableInterface currentFunctionValue, Map<Long, RandomVariableInterface> gradient) {
+    	
+    	double currentAccuracy = targetFunctionValue.sub(currentFunctionValue).div(uncertainties).squared()
+    			.getAverage(randomVariableFactory.createRandomVariable(1.0));
+    	
+    	// TODO: do this if-loop for every realization to improve performance
+    	if(currentAccuracy < getAccuracy())
 		{
-			accuracy	= currentAccuracy;
-			bestPoint	= nextPoint;
+			accuracy			= currentAccuracy;
+			bestParameterSet	= nextParameterSet;
+			
+			// decrease step size to approach minimum
+			lambda /= lambdaDivisor;
+		} else {
+			// increase step size to look for better place on surface
+			lambda *= lambdaMultiplicator;
 		}
-
-		// Calculate next point
-        nextPoint = nextPoint.addRatio(value,derivative);
-
-		numberOfIterations++;
-
+    	
+    	// estimate the delta for each parameter
+    	Map<Long, RandomVariableInterface> delta = estimateDelta(currentFunctionValue, gradient);
+    	
+    	// add the delta to each associated parameter
+    	for(Long key : nextParameterSet.keySet())
+    		nextParameterSet.put(key, nextParameterSet.get(key).add(delta.get(key)));
+  
+    	numberOfIterations++;
 	}
 
 	/* (non-Javadoc)
@@ -102,6 +178,10 @@ public abstract class LevenbergMarquardtSolver implements RandomVariableRootFind
 	public double getAccuracy() {
 		return accuracy;
 	}
+	
+	public double getLambda(){
+		return lambda;
+	}
 
 	/* (non-Javadoc)
 	 * @see net.finmath.rootfinder.RandomVariableRootFinderUsingDerivative#isDone()
@@ -111,12 +191,16 @@ public abstract class LevenbergMarquardtSolver implements RandomVariableRootFind
 		return isDone;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.finmath.rootfinder.RandomVariableRootFinderUsingDerivative#getBestPoint()
-	 */
 	@Override
-	public RandomVariableInterface getBestPoint() {
-		return bestPoint;
+	public TreeMap<Long, RandomVariableInterface> getNextParameters() {
+		return nextParameterSet;
 	}
+
+
+	@Override
+	public TreeMap<Long, RandomVariableInterface> getBestParameters() {
+		return bestParameterSet;
+	}
+
 
 }
