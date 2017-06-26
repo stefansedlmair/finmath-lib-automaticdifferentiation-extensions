f6a7ffebfbac564a3b7a55b20b3d11fe834daf3a
Stefan Sedlmair <stefan.sedlmair@campus.lmu.de>
2017-06-26 20:02:33 +0200

    ParameterJUnitTest for Levenberg Marquardt Class

---
+++ src/test/java/net/finmath/rootfinder/LevenbergMarquardtSolverTest.java
@@ -0,0 +1,126 @@
+/**
+ * 
+ */
+package net.finmath.rootfinder;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.TreeMap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import junit.framework.Assert;
+import net.finmath.montecarlo.AbstractRandomVariableFactory;
+import net.finmath.montecarlo.RandomVariable;
+import net.finmath.montecarlo.automaticdifferentiation.RandomVariableDifferentiableInterface;
+import net.finmath.montecarlo.automaticdifferentiation.backward.RandomVariableDifferentiableAADFactory;
+import net.finmath.montecarlo.automaticdifferentiation.backward.alternative.RandomVariableAADv2Factory;
+import net.finmath.optimizer.LevenbergMarquardt;
+import net.finmath.optimizer.SolverException;
+import net.finmath.randomnumbers.MersenneTwister;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * @author Stefan Sedlmair
+ *
+ */
+@RunWith(Parameterized.class)
+public class LevenbergMarquardtSolverTest {
+	
+	/* parameters specify the factories one wants to test against each other */
+	@Parameters
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new Object[][] {
+        	{new RandomVariableDifferentiableAADFactory()}
+        });
+    }
+
+    private final AbstractRandomVariableFactory randomVariableFactory;
+    
+    public LevenbergMarquardtSolverTest(AbstractRandomVariableFactory factory) {
+    	this.randomVariableFactory = factory;
+    	System.out.println(randomVariableFactory.getClass().getSimpleName());
+    }
+    
+	@Test
+	public void SimpleSolvingProblem() {
+	
+		/* --------------------- problem set up -------------------------*/
+		
+		long seed  = 1234;
+		MersenneTwister RNG = new MersenneTwister(seed);
+		
+		int numberOfRealization = (int)Math.pow(10, 6);
+		int numberOfArguments 	= 5;
+		
+		// create random vectors for arguments
+		TreeMap<Long, RandomVariableInterface> initialArguments = new TreeMap<>();
+		
+		double[] realizations = new double[numberOfRealization];
+		
+		// generate x argument
+		for(int j = 0; j < numberOfRealization; j++)
+			realizations[j] = RNG.nextDouble();
+		RandomVariableInterface x = new RandomVariable(0.0, realizations);
+		
+		// generate target function value
+		for(int j = 0; j < numberOfRealization; j++)
+			realizations[j] = RNG.nextDouble();
+		RandomVariableInterface targetFunctionValue = new RandomVariable(0.0, realizations);
+		
+		// generate arguments that shall be fitted
+		for(int i = 0; i < numberOfArguments; i++){
+			for(int j = 0; j < numberOfRealization; j++)
+				realizations[j] = RNG.nextDouble();
+			
+			RandomVariableDifferentiableInterface argument = (RandomVariableDifferentiableInterface) randomVariableFactory.createRandomVariable(0.0, realizations);
+			
+			initialArguments.put(argument.getID(), argument);
+		}
+		
+		/* --------------------- solver set up -------------------------*/
+		
+		
+		int maxNumberOfIterations = 100;
+		double targetAccuracy = Math.pow(10, -6);
+		
+		
+		LevenbergMarquardtSolver LMSolver = new LevenbergMarquardtSolver(initialArguments, targetFunctionValue);
+
+		RandomVariableDifferentiableInterface[] nextParametersArray = new RandomVariableDifferentiableInterface[numberOfArguments];
+
+		
+		while(LMSolver.getAccuracy() > targetAccuracy && LMSolver.getNumberOfIterations() < maxNumberOfIterations && Double.isFinite(LMSolver.getAccuracy())){
+			
+			TreeMap<Long, RandomVariableInterface> nextParameters = (TreeMap<Long, RandomVariableInterface>) LMSolver.getNextParameters();
+			
+			int argumentIndex = 0;
+			for(long key : nextParameters.keySet())
+				nextParametersArray[argumentIndex++] = (RandomVariableDifferentiableInterface) nextParameters.get(key);
+			
+			RandomVariableDifferentiableInterface currentFunctionValue = polynomialFuction(x, nextParametersArray);
+			
+			LMSolver.setValueAndDerivative(currentFunctionValue, currentFunctionValue.getGradient());
+			
+			System.out.println("Step#:    " + LMSolver.getNumberOfIterations());
+			System.out.println("Accuracy: " + LMSolver.getAccuracy());
+			System.out.println("Lambda:   " + LMSolver.getLambda());
+
+		}
+		
+		Assert.assertTrue(LMSolver.getAccuracy() < targetAccuracy );
+			
+	}
+	
+	private RandomVariableDifferentiableInterface polynomialFuction(RandomVariableInterface x, RandomVariableDifferentiableInterface[] a){
+		RandomVariableInterface polynom = a[0];
+		for(int i = 1; i < a.length; i++)
+			polynom = polynom.addProduct(a[i], x.pow(i));
+			
+		return (RandomVariableDifferentiableInterface) polynom;
+	}
+
+}
